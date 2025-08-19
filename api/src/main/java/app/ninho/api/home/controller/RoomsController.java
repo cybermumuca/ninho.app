9ebe0f6df8744748ed2c44c50200ff61c5642eb9
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2025-08-19 12:16:19 -0300

    refactor(api): check user scope before room creation

--- api/src/main/java/app/ninho/api/home/controller/RoomsController.java
+++ api/src/main/java/app/ninho/api/home/controller/RoomsController.java
@@ -5,6 +5,7 @@ import app.ninho.api.home.service.RoomsService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ public class RoomsController {
     }
 
     @PostMapping("/v1/rooms")
+    @PreAuthorize("hasAuthority('room:create')")
     public ResponseEntity<Void> addRoom(
         @Valid @RequestBody AddRoomRequest request,
         @AuthenticationPrincipal Jwt principal
--- api/src/main/java/app/ninho/api/home/service/RoomsService.java
+++ api/src/main/java/app/ninho/api/home/service/RoomsService.java
@@ -1,6 +1,7 @@
 package app.ninho.api.home.service;
 
 import app.ninho.api.auth.domain.User;
+import app.ninho.api.auth.repository.UserRepository;
 import app.ninho.api.home.domain.Room;
 import app.ninho.api.home.dto.AddRoomRequest;
 import app.ninho.api.home.repository.RoomRepository;
@@ -10,12 +11,23 @@ import org.springframework.stereotype.Service;
 public class RoomsService {
 
     private final RoomRepository roomRepository;
+    private final UserRepository userRepository;
 
-    public RoomsService(RoomRepository roomRepository) {
+    public RoomsService(RoomRepository roomRepository, UserRepository userRepository) {
         this.roomRepository = roomRepository;
+        this.userRepository = userRepository;
     }
 
     public void addRoom(AddRoomRequest request, String principalId) {
+        var principal = userRepository.findById(principalId)
+            .orElseThrow(() -> new IllegalArgumentException("User not found: " + principalId));
+
+        var principalHasPermission = principal.getScopes().stream().anyMatch(scope -> scope.getName().equals("room:create"));
+
+        if (!principalHasPermission) {
+            throw new IllegalArgumentException("User does not have permission to create rooms");
+        }
+
         var room = new Room();
         room.setName(request.name());
         room.setIcon(request.icon());
